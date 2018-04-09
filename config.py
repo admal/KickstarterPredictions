94a74fba9cff83e1ed12bc23a697d95bc7a4dbd0
admal <adam.malewski@wp.pl>
2018-04-10 00:15:55 +0200

    Project duration, data scaling, caterogical data to classes, more statistics info, data for training preparation

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 DATA_FILES = [
-    "C:\\Users\\ASUS\\Documents\\PW\\MSI\\KickstarterPredictions\\Data\\ks-projects-201612.csv",
-    "C:\\Users\\ASUS\\Documents\\PW\\MSI\\KickstarterPredictions\\Data\\ks-projects-201801.csv"
+    # "C:\\Users\\Adam\\Documents\\Materialy_pw\\PW\\MSI2\\Data\\ks-projects-201612.csv",
+    "C:\\Users\\Adam\\Documents\\Materialy_pw\\PW\\MSI2\\Data\\ks-projects-201801.csv"
 ]
 
-DATA_COLUMNES = [
-    'ID',
-    'name',
+DATA_COLUMNS = [
+    # 'ID', #just id
+    # 'name', # just name
     'category',
     'main_category',
     'currency',
     'deadline',
     'goal',
     'launched',
-    'pledged',
+    # 'pledged', # in initial state of a project there is no information about pledged money
     'state',
-    'backers',
+    # 'backers', # in initial state of a project there is no information about backers
     'country',
-    'usd pledged'
+    # 'usd pledged' # in initial state of a project there is no information about pledged money
 ]
